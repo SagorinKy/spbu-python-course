9211510fe327e8b537a0083dc4ff89de6fcb499e
Kirill Sagorin <st128530@student.spbu.ru>
2025-10-03 18:48:46 +0300

    corrected errors

--- project/matrices.py
+++ project/matrices.py
@@ -1,14 +1,16 @@
-from vectors import scalar_product
+from .vectors import scalar_product
 
-def matrices_sum (matrix_1 : list[list], matrix_2 : list[list]) -> list[list]:
+
+def matrices_sum(matrix_1: list[list], matrix_2: list[list]) -> list[list]:
     """Adds to the elements of first matrix values from the corresponding elements of second one"""
-    
+
     for i in range(min(len(matrix_1), len(matrix_2))):
-        for j in range(min(len(matrix_1[i]), len(matrix_2)[i])):
+        for j in range(min(len(matrix_1[i]), len(matrix_2[i]))):
             matrix_1[i][j] += matrix_2[i][j]
     return matrix_1
 
-def matrices_product (matrix_1 : list[list], matrix_2 : list[list]) -> list[list]:
+
+def matrices_product(matrix_1: list[list], matrix_2: list[list]) -> list[list]:
     """
     if raw number of first matrix is equal to line number in second matrix
     return their product
@@ -24,6 +26,7 @@ def matrices_product (matrix_1 : list[list], matrix_2 : list[list]) -> list[list
         return result_matrix
     return None
 
-def transpose (matrix : list[list]) -> list[list]:
+
+def transpose(matrix: list[list]) -> list[list]:
     """Transpose a matrix"""
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
--- project/vectors.py
+++ project/vectors.py
@@ -1,28 +1,33 @@
 from math import acos
 
-def vector_length (vector : list) -> float:
-    """ Calculates vector's length by formulae ((x_1)^2 + ... + (x_n)^2)^0.5 """
+
+def vector_length(vector: list) -> float:
+    """Calculates vector's length by formulae ((x_1)^2 + ... + (x_n)^2)^0.5"""
     length_squared = 0
     for element in vector:
-        length_squared += element ** 2
-    return length_squared ** 0.5
+        length_squared += element**2
+    return length_squared**0.5
 
-def scalar_product (vector_1 : list, vector_2 : list) -> float:
+
+def scalar_product(vector_1: list, vector_2: list) -> float:
     """
     Calculates scalar product of two vectors
     If one is longer than another one we assume all missing elements are 0
     """
 
     result = 0
-    for i in range (0, min(len(vector_1), len(vector_2))):
+    for i in range(0, min(len(vector_1), len(vector_2))):
         result += vector_1[i] * vector_2[i]
     return result
 
-def angle_between_vectors (vector_1 : list, vector_2 : list) -> float:
+
+def angle_between_vectors(vector_1: list, vector_2: list) -> float:
     """
     Calculates angle between vectors by formulae
     angle = arccos(scalar product / product of vector length)
     """
-    
-    cos = scalar_product(vector_1, vector_2) / (vector_length(vector_1) * vector_length(vector_2))
+
+    cos = scalar_product(vector_1, vector_2) / (
+        vector_length(vector_1) * vector_length(vector_2)
+    )
     return acos(cos)
