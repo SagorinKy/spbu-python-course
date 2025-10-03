14b04d3459a0f827a2e3ca49efdf825e530fc00e
Kirill Sagorin <st128530@student.spbu.ru>
2025-10-03 20:03:53 +0300

    devided tests into file for metrices and for vectors

---
+++ tests/test_matrices.py
@@ -0,0 +1,19 @@
+import pytest
+import project.matrices as m
+
+
+def test_matrices_sum():
+    matrix_1 = [[1, 2, 3], [1, 2, 3]]
+    matrix_2 = [[8, 6, 4], [4, 2, 0]]
+    assert m.matrices_sum(matrix_1, matrix_2) == [[9, 8, 7], [5, 4, 3]]
+
+
+def test_matrices_product():
+    matrix_1 = [[1, 2, 3], [3, 2, 1]]
+    matrix_2 = [[8, 6], [4, 4], [2, 0]]
+    assert m.matrices_product(matrix_1, matrix_2) == [[22, 14], [34, 26]]
+
+
+def test_transpose():
+    matrix = [[5, 6, 7], [22, 34, 55], [1, 1, 1]]
+    assert m.transpose(matrix) == [[5, 22, 1], [6, 34, 1], [7, 55, 1]]
