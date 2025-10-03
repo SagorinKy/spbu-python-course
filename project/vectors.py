c379792c9b7e5517814d31bb604e94c3a98f8b2c
Kirill Sagorin <st128530@student.spbu.ru>
2025-10-03 15:18:38 +0300

    added docstring to two modules

--- project/vectors.py
+++ project/vectors.py
@@ -8,11 +8,21 @@ def vector_length (vector : list) -> float:
     return length_squared ** 0.5
 
 def scalar_product (vector_1 : list, vector_2 : list) -> float:
+    """
+    Calculates scalar product of two vectors
+    If one is longer than another one we assume all missing elements are 0
+    """
+
     result = 0
     for i in range (0, min(len(vector_1), len(vector_2))):
         result += vector_1[i] * vector_2[i]
     return result
 
 def angle_between_vectors (vector_1 : list, vector_2 : list) -> float:
+    """
+    Calculates angle between vectors by formulae
+    angle = arccos(scalar product / product of vector length)
+    """
+    
     cos = scalar_product(vector_1, vector_2) / (vector_length(vector_1) * vector_length(vector_2))
     return acos(cos)
