9ee6b2b2c82dfdd48c64ba44ed2bfca192d7e031
Kirill Sagorin <st128530@student.spbu.ru>
2025-10-09 22:20:48 +0300

    result: Union[float, int] = 0

--- project/task_1/vectors.py
+++ project/task_1/vectors.py
@@ -3,7 +3,7 @@ from typing import List
 from typing import Union
 
 
-def vector_length(vector: List[Union[float, int]]) -> float:
+def vector_length(vector: List[Union[float, int]]) -> Union[float, int]:
     """
     Calculates vector's length by formulae ((x_1)^2 + ... + (x_n)^2)^0.5
 
@@ -12,7 +12,7 @@ def vector_length(vector: List[Union[float, int]]) -> float:
     Returns:
         float: vector length
     """
-    length_squared = 0
+    length_squared: Union[float, int] = 0
     for element in vector:
         length_squared += element**2
     return length_squared**0.5
@@ -20,7 +20,7 @@ def vector_length(vector: List[Union[float, int]]) -> float:
 
 def scalar_product(
     vector_1: List[Union[float, int]], vector_2: List[Union[float, int]]
-) -> float:
+) -> Union[float, int]:
     """
     Calculates scalar product of two vectors
     If one is longer than another one we assume all missing elements are 0
@@ -32,7 +32,7 @@ def scalar_product(
         float, int: scalar product
     """
 
-    result = 0
+    result: Union[float, int] = 0
     for i in range(0, min(len(vector_1), len(vector_2))):
         result += vector_1[i] * vector_2[i]
     return result
@@ -40,7 +40,7 @@ def scalar_product(
 
 def angle_between_vectors(
     vector_1: List[Union[float, int]], vector_2: List[Union[float, int]]
-) -> float:
+) -> Union[float, int]:
     """
     Calculates angle between vectors by formulae
     angle = arccos(scalar product / product of vector length)
@@ -53,7 +53,7 @@ def angle_between_vectors(
         float, int: angle between vectors
     """
 
-    cos = scalar_product(vector_1, vector_2) / (
+    cos: Union[float, int] = scalar_product(vector_1, vector_2) / (
         vector_length(vector_1) * vector_length(vector_2)
     )
     return acos(cos)
